3771786dee5a7bc120abc7836f9fa099c7afe5be
Vidoje <vidoje.sevic.275.19@ict.edu.rs>
2024-01-03 22:00:31 +0100

    Tests: Test coverage complete in Stacks

--- tests/unit/test_stacks.c
+++ tests/unit/test_stacks.c
@@ -13,3 +13,55 @@ Test(test_create_stack, basic_test) {
 
     destroy_stack(stack);
 }
+
+Test(test_stack_size, test) {
+    Stack *stack = create_stack();
+
+    cr_assert_eq(stack_size(stack), 0, "Size should be 0");
+
+    stack_push(stack, 4);
+    stack_push(stack, 7);
+
+    cr_assert_eq(stack_size(stack), 2, "Size should be 0");
+
+    destroy_stack(stack);
+}
+
+Test(test_stack_peek, test) {
+    Stack *stack = create_stack();
+    bool status = false;
+
+    cr_assert_eq(stack_peek(stack, &status), -1, "Stack peek should return -1 (NULL)");
+
+    for (int i = 0; i < 5; ++i) {
+        stack_push(stack, i);
+        cr_assert_eq(stack_peek(stack, &status), i, "Stack peek should return %d", i);
+    }
+
+    destroy_stack(stack);
+}
+
+Test(test_stack_push, test) {
+    Stack *stack = create_stack();
+
+    stack_push(stack, 2);
+    cr_assert_eq(stack_size(stack), 1, "Should be 1");
+    stack_push(stack, 6);
+    cr_assert_eq(stack_size(stack), 2, "Should be 2");
+
+    destroy_stack(stack);
+}
+
+Test(test_stack_pop, test) {
+    Stack *stack = create_stack();
+    bool status = false;
+
+    cr_assert_eq(stack_pop(stack, &status), -1, "Stack pop should return -1 (NULL)");
+    int test_val = 5;
+    stack_push(stack, test_val);
+    cr_assert_eq(stack_size(stack), 1, "Stack size should be 1");
+    cr_assert_eq(stack_pop(stack, &status), test_val, "Stack pop should return last pushed value");
+    cr_assert_eq(stack_size(stack), 0, "Stack size should be 0");
+
+    destroy_stack(stack);
+}
